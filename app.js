03b348c637d27ccadd2a99b25c4b763a48a09c11
ammarshaikh123 <144041358+ammarshaikh22@users.noreply.github.com>
2024-03-24 23:43:55 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const randDom_Num = Math.round(Math.random() * 5 + 1);
+const result = await inquirer.prompt([
+    { name: "output_user", type: "number", message: "Guess the number form 1-6:" }
+]);
+if (result.output_user === randDom_Num) {
+    console.log(`You win to guess the number it gone same ${result.output_user}:${randDom_Num}`);
+}
+else {
+    console.log(`You lose to guess the number that is ${randDom_Num}`);
+}
